79fd298d8cdf63a1ed55c2bae740219fc74aee6b
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2021-11-13 22:00:08 -0300

    Primeiro commit. Criar projeto Curso Spring Boot

---
+++ src/main/java/br/com/zup/Curso/Spring/Boot/CursoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.Curso.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootApplication.class, args);
+	}
+
+}
